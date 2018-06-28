084ae38821ad6e5317ea434276db0d7da1b1297b
Kofi Oghenerukevwe <rukykf@gmail.com>
2018-06-28 14:56:43 +0100

    init: setup initial application files

---
+++ itemcatalog/dbmodels.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from itemcatalog.db import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), nullable=False)
+
+class Password(db.Model):
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, primary_key=True)
+    salt = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False, unique=True)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text)
+    categoryid = db.Column(db.Integer, db.ForeignKey("category.id"))
+    # category = db.relationship("Category")
+
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"))
+    # user = db.relationship("user")
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text, nullable=False)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    pubdate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    itemid = db.Column(db.Integer, db.ForeignKey("item.id"))
+
+class Like(db.Model):
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, primary_key=True)
+    itemid = db.Column(db.Integer, db.ForeignKey("item.id"), nullable=False, primary_key=True)
---
+++ tests/testdata.py
@@ -0,0 +1,6 @@
+from itemcatalog.db import db
+from itemcatalog.dbmodels import User, Item, Category, Like, Comment, Password
+
+
+def create_test_data():
+    return "hello"
